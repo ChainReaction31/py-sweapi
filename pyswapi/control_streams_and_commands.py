08700c27dee164bf2f21255f3c47d914be441c97
Ian Patterson <cr31.dev@gmail.com>
2023-03-28 17:31:18 -0500

    [Docs] Add missing docstrings for ControlInterface
    Signed-off-by: Ian Patterson <cr31.dev@gmail.com>

--- pyswapi/control_streams_and_commands.py
+++ pyswapi/control_streams_and_commands.py
@@ -1,5 +1,4 @@
 from dataclasses import dataclass
-from datetime import datetime
 from enum import Enum
 
 from oshdatacore.component_implementations import DataRecordComponent
@@ -34,6 +33,9 @@ class Command:
 
 
 class CommandStatusCode(Enum):
+    """
+    Defines accepted command status codes
+    """
     PENDING = 'PENDING'
     ACCEPTED = 'ACCEPTED'
     REJECTED = 'REJECTED'
@@ -45,23 +47,32 @@ class CommandStatusCode(Enum):
     FAILED = 'FAILED'
 
 
-# @dataclass(kw_only=True)
-# class CommandStatus:
-#     command_id: str
-#     report_time: datetime
-#     status: CommandStatusCode.value
-
-
 @dataclass(kw_only=True)
 class ControlInterface:
+    """
+    Control interfaces define the commands that can be issued to a system.
+    :param name: The machine name of the control interface
+    :param input_name: The human-readable name of the control interface
+    :param __command_schema: The schema of the commands that can be issued to the system, often a reflection of the
+    system's data model
+    """
     name: str
     input_name: str
     __command_schema: DataRecordComponent = None
 
     def add_schema(self, component: DataRecordComponent):
+        """
+        Adds a schema to the control interface
+        :param component: The DataRecordComponent that defines the names and types of the commands that can be issued to
+        the system
+        """
         self.__command_schema = component
 
     def insert_control_stream(self, system: System):
+        """
+        Inserts the control stream into the system
+        :param system: The system to which the control stream will be added
+        """
         post_system_controls(system.get_node_api_url(), system.get_sys_id(), self.__get_control_stream_dict())
 
     def __get_control_stream_dict(self):
@@ -72,5 +83,3 @@ class ControlInterface:
                 'commandSchema': self.__command_schema.datastructure_to_dict()
             }
         }
-
-    # def set):
